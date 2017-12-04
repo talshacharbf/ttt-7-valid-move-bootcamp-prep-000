78952abc03f0ae346aa2bfb6bda8a6fbbbb74c80
tal shachar <tal.shachar@buzzfeed.com>
2017-12-04 07:10:47 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(index)
   if index.between(0,8)
     position_taken?.index
   else
-  "false"  
+  return "false"
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
